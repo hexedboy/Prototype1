693bf5143cfc19759cc8ad283f5cb7f21a7a73ab
hexedboy <64075822+hexedboy@users.noreply.github.com>
2020-05-04 11:14:55 +0200

    Movement

---
+++ Prototype1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float smoothSpeed = 0.125f;
+    [SerializeField] Vector3 followOffset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPos = target.position + followOffset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPos;
+        transform.LookAt(target);
+    }
+}
---
+++ Prototype1/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] float jumpForce;
+    [SerializeField] Vector3 movementForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("z"))
+        {
+            this.GetComponent<Rigidbody>().AddForce(0, jumpForce, 0, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.GetComponent<Rigidbody>().AddForce(movementForce.x * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.GetComponent<Rigidbody>().AddForce(-movementForce.x * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.GetComponent<Rigidbody>().AddForce(0, 0, movementForce.z * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.GetComponent<Rigidbody>().AddForce(0, 0, -movementForce.z * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+
+    
+}
